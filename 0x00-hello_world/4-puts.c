d459ea50aaeb21943c7894a66ca7141f832cf036
m-morsi201 <muhammadmorsy5@gmail.com>
2023-06-15 19:20:31 -0700

    fix erro

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 int main(void)
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 {
- puts("Programming is like building a multilingual puzzle/n");
- return (0);
+	 puts("\"Programming is like building a multilingual puzzle");
+	 return (0);
 }

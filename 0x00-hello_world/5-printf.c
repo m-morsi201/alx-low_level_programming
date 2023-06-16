11e9dc2f41450ded1ad2a104559a2778c139700f
m-morsi201 <muhammadmorsy5@gmail.com>
2023-06-15 20:29:06 -0700

    fix error

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-int main(void)
 
 /**
  * main - Entry point
  *
+ *Description:  a C program that prints with printf function
+ *
  * Return: Always 0 (Success)
 */
 
+int main(void)
 {
 	print("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);

a03beee4fb82d5af09be9313ea0d32d2058dc543
m-morsi201 <muhammadmorsy5@gmail.com>
2023-06-16 17:55:34 -0700

    a to z expect e, q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a to z
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char aph = 'a';
+
+	while (aph <= 'z')
+	{
+		if (aph == 'e' || aph == 'q')
+		putchar(aph);
+		aph++;
+	}
+	putchar('\n');
+	return (0);
+}

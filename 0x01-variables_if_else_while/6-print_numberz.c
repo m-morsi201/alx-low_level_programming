e6384d08960b3ceb9a477610132c8a0b4bc68f5e
m-morsi201 <muhammadmorsy5@gmail.com>
2023-06-16 18:24:13 -0700

    char 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <strio.h>
+
+/**
+ * main - entry point
+ *
+ * Description:  positive or negative
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void);
+{
+	int m =	0;
+
+	while (m <= 9)
+	{
+		putchart(m + '0');
+		m++;
+	}
+	putchar('\n');
+
+	return (0);
+}

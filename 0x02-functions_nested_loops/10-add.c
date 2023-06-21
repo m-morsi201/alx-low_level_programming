405d74fd9660ebab17999e4e2fafa7c691e93324
m-morsi201 <muhammadmorsy5@gmail.com>
2023-06-20 19:59:58 -0700

    adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add -  adds two integers and returns the result.
+ *
+ * @R:frist input.
+ *
+ * @M: socsnd input .
+ *
+ * Return: sum of R + M
+*/
+
+int add(int R, int M)
+{
+	int sum = R + M;
+
+	return (sum);
+}

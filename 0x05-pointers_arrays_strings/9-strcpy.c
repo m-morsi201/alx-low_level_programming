ca5434958cc55836f93e5be742330e11704469dd
m-morsi201 <muhammadmorsy5@gmail.com>
2023-06-26 19:37:07 -0700

    function that copies the string pointed

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcpy -  that copies the string pointed.
+ * @dest: is a input.
+ * @src: is a input.
+ * Return: dest.
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int s
+		s = -1;
+	do {
+		s++;
+		dest[s] = scr[s];
+	} while (scr[s] != '\0');
+
+	return (dest);
+}

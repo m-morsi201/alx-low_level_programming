a45ed9447105ab24f34241a6cc3195eddca985e2
m-morsi201 <muhammadmorsy5@gmail.com>
2023-06-29 17:42:15 -0700

    copy two strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,8 +12,8 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int m;
 
-	for (m = 0; m < n && src[i] != '\0'; m++)
-		dest[m] = src[i];
+	for (m = 0; m < n && src[m] != '\0'; m++)
+		dest[m] = src[m];
 
 	while (m < n)
 	{

452f15d56f6b2693244126e8b77aba273c4e5c7a
m-morsi201 <muhammadmorsy5@gmail.com>
2023-06-29 17:40:49 -0700

    copy two strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strncpy -  function that copy two strings.
+ * @dest: is an input.
+ * @src: is an input.
+ * @n: is number of char.
+ * Return: string of dest.
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int m;
+
+	for (m = 0; m < n && src[i] != '\0'; m++)
+		dest[m] = src[i];
+
+	while (m < n)
+	{
+		dest[m] = '\0';
+		m++;
+	}
+	return (dest);
+}

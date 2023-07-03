8c49366ccdeb9818efa9daca7f495ee63af7fa47
m-morsi201 <muhammadmorsy5@gmail.com>
2023-07-03 16:06:28 -0700

    fix 'NULL' to '\0'

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,12 @@ char *_strchr(char *s, char c)
 {
 	int m;
 
-	for (m = 0; s[m] >= 'NULL'; m++)
+	for (m = 0; s[m] >= '\0'; m++)
 	{
 		if (s[m] == c)
 		{
 			return (s + m);
 		}
 	}
-	return ('NULL');
+	return ('\0');
 }

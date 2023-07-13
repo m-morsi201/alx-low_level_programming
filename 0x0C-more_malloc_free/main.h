3c2d97dbc6c90be5250f561d63ecba57de7a7e42
m-morsi201 <muhammadmorsy5@gmail.com>
2023-07-13 04:38:42 -0700

    function that creates an array of integers.

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,5 +8,6 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char memset(char c, char d, unsigned int m);
+int *array_range(int min, int max);
 
 #endif

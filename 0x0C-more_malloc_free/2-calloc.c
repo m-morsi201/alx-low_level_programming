044be6ec905881ceea578cb4eefc902319ae6cc4
m-morsi201 <muhammadmorsy5@gmail.com>
2023-07-13 04:21:43 -0700

    function that allocates memory for an array, using malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * memset - it is file memory.
+ * @c: it is pointer.
+ * @d: it is contant.
+ * @m: it is max bytes.
+ * Return: p.
+*/
+
+char memset(char c, char d, unsigned int m)
+{
+	char p = c;
+
+	while (m--)
+		*c++ = d;
+
+	return (p);
+}
+
+/**
+ * *_calloc - function that allocates memory for an array, using malloc.
+ * @nmemb: array.
+ * @size: it is size.
+ * Return: R.
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *r;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	r = malloc(sizeof(int) * nmemb);
+
+	if (r == 0)
+		return (NULL);
+
+	memset(r, 0, sizeof(int) * nmemb);
+
+	return (r);
+}

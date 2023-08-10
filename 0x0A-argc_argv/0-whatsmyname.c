5612fe72fe6da6a9fdd0de14367c57b1382c2b08
Muhamad <muhammadmorsy5@gmail.com>
2023-08-09 17:42:02 -0700

    Write a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - it is a func to print prog name.
+ * @argc: it is int.
+ * @argc: it is array.
+ * Return: (0).
+*/
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}

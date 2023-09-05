1727755ffdbfa1e8c25591aaa5638b983be1619d
m-morsi201 <muhammadmorsy5@gmail.com>
2023-09-04 17:22:02 -0700

    edit main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+#define READ_BUF_SIZE 1024
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 
 ssize_t read_textfile(const char *filename, size_t letters);
 

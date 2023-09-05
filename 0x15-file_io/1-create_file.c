cb408bea76d7a3d27d1289a9f52d0906be445599
m-morsi201 <muhammadmorsy5@gmail.com>
2023-09-04 17:57:11 -0700

    fix code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _strlen - it is a function to get lens of string.
+ * @s:it is a string.
+ * Return: lenth of string.
+*/
+
+ssize_t _strlen(const char *s)
+{
+	ssize_t e = 0;
+
+	if (!s)
+		return (-1);
+
+	while (*s++)
+		++e;
+
+	return (e);
+}
+
+/**
  * create_file - it is a function that creates a file.
  * @filename: it is a name of the file to create.
  * @text_content:  is a NULL terminated string to write to the file.
@@ -9,23 +28,25 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int t, f;
+	ssize_t bw;
+	int f;
 
-	if (filename == NULL)
+	bw = 0;
+
+	if (!filename)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
+	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
-	while (text_content[t] != '\0')
-		t++;
-
-	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (f == -1)
+	if (f < 0)
 		return (-1);
 
-	write(f, text_content, t);
+	if (text_content)
+		b_written = write(f, text_content, _strlen(text_content));
 
+	close(f);
+
+	if (bw < 0)
+		return (-1);
 	return (1);
 }
